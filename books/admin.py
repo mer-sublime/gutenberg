16669fb41987b35396067f985e2be68c8673bf7a
Rémi <contact@remiblemus.com>
2024-10-14 18:25:03 +0200

    Add books.models and admin

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from books.models import Person, Language, Book
+
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Language)
+admin.site.register(Book)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=128)
+    birth_year = models.SmallIntegerField(blank=True, null=True)
+    death_year = models.SmallIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Language(models.Model):
+    code = models.CharField(max_length=4, unique=True)
+
+    def __str__(self):
+        return self.code
+
+
+class Book(models.Model):
+    title = models.CharField(blank=True, max_length=1024, null=True)
+    authors = models.ManyToManyField(Person)
+    translators = models.ManyToManyField(Person, related_name='books_translated')
+    languages = models.ManyToManyField(Language)
+    media_type = models.CharField(max_length=16)
+    copyright = models.BooleanField(null=True)
+    download_count = models.IntegerField()
+    subjects_json = models.JSONField()
+    bookshelves_json = models.JSONField()
+    formats_json = models.JSONField()
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        else:
+            return str(self.id)
